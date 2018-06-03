18d80580ec7ff54e0938500bfeb1f938f690c8da
SteamPisya <mr.bart9822@gmail.com>
2018-06-03 22:12:26 +0700

    Game

---
+++ game/offline.js
@@ -0,0 +1,21 @@
+﻿{
+	"version": 1528023646,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/btvk-sheet0.png",
+		"images/btwhat-sheet0.png",
+		"images/btfon-sheet0.png",
+		"images/bg-sheet0.png",
+		"images/vkcom-sheet0.png",
+		"images/btstart-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
