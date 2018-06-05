5dff785bd136a7a4e28e27792f8d029c95da6b5d
SteamPisya <mr.bart9822@gmail.com>
2018-06-05 15:09:48 +0700

    upg Game

--- Gametest/offline.js
+++ Gametest/offline.js
@@ -1,5 +1,5 @@
 ﻿{
-	"version": 1528181843,
+	"version": 1528186148,
 	"fileList": [
 		"data.js",
 		"c2runtime.js",
@@ -10,7 +10,14 @@
 		"images/sprite-sheet2.png",
 		"images/green-sheet0.png",
 		"images/red-sheet0.png",
-		"images/Ресурс-sheet0.png",
+		"images/cursor-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"images/sprite2-sheet1.png",
+		"images/sprite2-sheet2.png",
+		"images/sprite2-sheet3.png",
+		"images/sprite2-sheet4.png",
+		"images/sprite2-sheet5.png",
+		"images/tiledbackground.png",
 		"icon-16.png",
 		"icon-32.png",
 		"icon-114.png",
