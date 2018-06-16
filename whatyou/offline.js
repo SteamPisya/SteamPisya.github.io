483aa512c28cc3d8ebfb86e46d95fd5c3714bbd4
SteamPisya <mr.bart9822@gmail.com>
2018-06-16 16:21:36 +0700

    Revert "shop"
    This reverts commit d78ab1100f6a326b0717bb47b4e93eab27b8cee6.

---
+++ whatyou/offline.js
@@ -0,0 +1,30 @@
+﻿{
+	"version": 1528364577,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/sprite-sheet0.png",
+		"images/sprite-sheet1.png",
+		"images/sprite-sheet2.png",
+		"images/green-sheet0.png",
+		"images/red-sheet0.png",
+		"images/cursor-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"images/sprite2-sheet1.png",
+		"images/sprite2-sheet2.png",
+		"images/sprite2-sheet3.png",
+		"images/sprite2-sheet4.png",
+		"images/sprite2-sheet5.png",
+		"images/tiledbackground.png",
+		"images/sprite3-sheet0.png",
+		"images/button-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
